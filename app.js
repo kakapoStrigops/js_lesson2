d7a5b55496c7fb6e2c3ec83f7d2f4303ea1abd7c
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-29 08:45:55 +0000

    条件分岐

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
-// 変数の書き方
+// // 変数の書き方
 
-let hello = 'Hello World';
-alert(hello)
+// let hello = 'Hello World';
+// alert(hello)
 
-// let int1 = 1;
+// // let int1 = 1;
 
-// let int2 = -10;
+// // let int2 = -10;
 
-// let float1 = 3.14;
+// // let float1 = 3.14;
 
-// let str1 = 'JavaScriptを覚えよう';
+// // let str1 = 'JavaScriptを覚えよう';
 
 
-// 四則演算
-alert(4 + 3);
+// // 四則演算
+// alert(4 + 3);
 
-alert(8 - 5);
+// alert(8 - 5);
 
-alert(2 * 6);
+// alert(2 * 6);
 
-alert(10 / 2);
+// alert(10 / 2);
 
-// 文字列の結合
-alert('Hello' + 'World');
+// // 文字列の結合
+// alert('Hello' + 'World');
 
-let str1 = 'Hello';
-let str2 = 'World!!';
-alert(str1 + str2);
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// alert(str1 + str2);
+
+// 条件分岐
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple) {
+  alert("みかんとりんごが同じ値段")
+} else {
+  alert('みかんの値段がりんごより高い')
+}
+
 
