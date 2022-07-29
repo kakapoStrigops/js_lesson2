467ff430452bd9c1173aa00da96f74bd56eae62f
EC2 Default User <ec2-user@ip-172-31-12-99.ap-northeast-1.compute.internal>
2022-07-29 08:40:19 +0000

    四則演算／文字列の結合

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
+// 変数の書き方
+
 let hello = 'Hello World';
 alert(hello)
+
+// let int1 = 1;
+
+// let int2 = -10;
+
+// let float1 = 3.14;
+
+// let str1 = 'JavaScriptを覚えよう';
+
+
+// 四則演算
+alert(4 + 3);
+
+alert(8 - 5);
+
+alert(2 * 6);
+
+alert(10 / 2);
+
+// 文字列の結合
+alert('Hello' + 'World');
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
+
